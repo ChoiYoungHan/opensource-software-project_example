c9735b831cfc07c7363558b07e791c39c51c8828
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-04-13 16:37:56 +0900

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,7 @@ public class Main{
 		System.out.println("Hello World!");
 		System.out.println("Good");
 	        System.out.println("Example");
+		
+		System.out.println("Modification in the Github");
 	}
 }
